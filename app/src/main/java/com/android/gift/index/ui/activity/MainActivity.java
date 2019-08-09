7f3f31b7d13719816a4cba1ab0d5e62b290e690e
Yuye <584312311@qq.com>
2019-08-09 20:05:04 +0800

    github主页修改

--- app/src/main/java/com/android/gift/index/ui/activity/MainActivity.java
+++ app/src/main/java/com/android/gift/index/ui/activity/MainActivity.java
@@ -342,7 +342,7 @@ public class MainActivity extends AppCompatActivity implements RoundGlobeView.On
             //Github主页
             if(TextUtils.equals(tag,ITEM_HOME)){
                 try {
-                    Uri uri=Uri.parse("https://github.com/Yuye584312311/iLive");
+                    Uri uri=Uri.parse("https://github.com/Yuye584312311/Live");
                     Intent intent=new Intent(Intent.ACTION_VIEW,uri);
                     startActivity(intent);
                 }catch (RuntimeException e){

ab98c861532dfdbebc886f0f8fcb0e277d105371
Yuye <584312311@qq.com>
2019-09-11 09:30:08 +0800

    不同直播间类型框架预封装

---
+++ app/src/main/java/com/android/gift/base/BaseRoomActivity.java
@@ -0,0 +1,178 @@
+package com.android.gift.base;
+
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
+import com.android.gift.R;
+import com.android.gift.bean.UserInfo;
+import com.android.gift.constant.Constants;
+import com.android.gift.gift.dialog.LiveGiftDialog;
+import com.android.gift.room.bean.CustomMsgInfo;
+import com.android.gift.room.doalog.InputKeyBoardDialog;
+import com.android.gift.util.ScreenLayoutChangedHelp;
+
+/**
+ * Created by TinyHung@outlook.com
+ * 2019/9/11
+ * 所有类型直播间父类
+ */
+
+public class BaseRoomActivity<C extends RoomBaseController> extends AppCompatActivity {
+
+    //直播间控制器
+    protected C mController;
+    //输入框高度监听
+    private ScreenLayoutChangedHelp mLayoutChangedListener;
+    //输入框
+    private InputKeyBoardDialog mInputTextMsgDialog;
+    //礼物面板
+    protected LiveGiftDialog mGiftDialog;
+    //房间ID
+    protected String mRoomid;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+        //保持屏幕高亮，不锁屏
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        //软键盘高度检测
+        mLayoutChangedListener= ScreenLayoutChangedHelp.get(BaseRoomActivity.this)
+                .setOnSoftKeyBoardChangeListener(new ScreenLayoutChangedHelp.OnSoftKeyBoardChangeListener() {
+                    @Override
+                    public void keyBoardShow(int height) {
+                        if(null!=mController) mController.showInputKeyBord(true,height);
+                    }
+
+                    @Override
+                    public void keyBoardHide(int height) {
+                        if(null!=mController) mController.showInputKeyBord(false,height);
+                    }
+                });
+    }
+
+    /**
+     * 直播间初始化，子类初始化调用
+     */
+    protected void initRoomViews(){
+        if(null!=mController){
+            //初始化直播间控制器
+            RelativeLayout videoController = (RelativeLayout) findViewById(R.id.video_controller);
+            videoController.addView(mController);
+        }
+    }
+
+    protected void showGiftBoard(UserInfo anchorUser){
+        if(null==mGiftDialog){
+            mGiftDialog = LiveGiftDialog.getInstance(BaseRoomActivity.this);
+            mGiftDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                @Override
+                public void onDismiss(DialogInterface dialog) {
+                    if(null!=mController){
+                        mController.closeGiftBoard();
+                    }
+                }
+            });
+        }
+        mGiftDialog.setReceiveUserInfo(anchorUser)
+                .setReceiveRoomID(mRoomid)
+                .setAutoSelectedEnable(true)
+                .show();
+        if(null!=mController){
+            mController.showGiftBoard();
+        }
+    }
+    /**
+     * 发消息弹出框
+     */
+    protected void showInputMsgDialog() {
+        if(null==mInputTextMsgDialog){
+            //输入框
+            mInputTextMsgDialog = InputKeyBoardDialog.getInstance(this)
+                    .setBackgroundWindown(0.0f)
+                    .setHintText("请输入聊天内容")
+                    .setMode(1)
+                    .setOnActionFunctionListener(new InputKeyBoardDialog.OnActionFunctionListener() {
+                        //提交发射
+                        @Override
+                        public void onSubmit(String content, boolean tanmuOpen) {
+                            CustomMsgInfo customMsgInfo=new CustomMsgInfo(0);
+                            customMsgInfo.setChildCmd(Constants.MSG_CUSTOM_TEXT);
+                            customMsgInfo.setMsgContent(content);
+                            customMsgInfo.setTanmu(tanmuOpen);
+                            if(null!=mController){
+                                mController.onNewTextMessage(customMsgInfo,false);
+                            }
+                        }
+                        //告知输入框是否可发射状态
+                        @Override
+                        public boolean isAvailable() {
+                            return true;
+                        }
+                    });
+            mInputTextMsgDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                @Override
+                public void onDismiss(DialogInterface dialog) {
+                    if(null!=mController) mController.showInputKeyBord(false,0);
+                }
+            });
+            WindowManager windowManager = getWindowManager();
+            Display display = windowManager.getDefaultDisplay();
+            WindowManager.LayoutParams lp = mInputTextMsgDialog.getWindow().getAttributes();
+            lp.width = (int) (display.getWidth()); //设置宽度
+            mInputTextMsgDialog.getWindow().setAttributes(lp);
+            mInputTextMsgDialog.setCancelable(true);
+            mInputTextMsgDialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+        }
+        mInputTextMsgDialog.show();
+    }
+
+    /**
+     * 收到新的消息交给控制器统一处理刷新
+     * 播放礼物动画
+     * @param customMsgInfo
+     * @param isSystemPro   是否来系统推送的
+     */
+    protected void newSystemCustomMessage(CustomMsgInfo customMsgInfo, boolean isSystemPro) {
+        if (null == customMsgInfo) return;
+        if (null != mController) mController.newSystemCustomMessage(customMsgInfo, isSystemPro);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(null!= mLayoutChangedListener) {
+            mLayoutChangedListener.onDestroy();
+            mLayoutChangedListener=null;
+        }
+        if(null!=mInputTextMsgDialog){
+            mInputTextMsgDialog.destroy();
+            mInputTextMsgDialog.dismiss();
+            mInputTextMsgDialog=null;
+        }
+        //控制器机内部销毁，礼物动画回收也在此方法内部进行
+        if(null!=mController){
+            mController.onDestroy();
+            mController=null;
+        }
+        if(null!=mGiftDialog&&mGiftDialog.isShowing()){
+            mGiftDialog.destroy();
+            mGiftDialog.dismiss();
+        }
+        mGiftDialog=null;
+    }
+}
--- app/src/main/java/com/android/gift/room/activity/LiveRoomActivity.java
+++ app/src/main/java/com/android/gift/room/activity/LiveRoomActivity.java
@@ -1,30 +1,23 @@
 package com.android.gift.room.activity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Display;
-import android.view.View;
-import android.view.Window;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 import com.android.gift.R;
+import com.android.gift.base.BaseRoomActivity;
 import com.android.gift.bean.UserInfo;
 import com.android.gift.constant.Constants;
-import com.android.gift.gift.dialog.LiveGiftDialog;
 import com.android.gift.gift.manager.GiftBoardManager;
 import com.android.gift.manager.VibratorManager;
 import com.android.gift.room.bean.CustomMsgExtra;
 import com.android.gift.room.bean.CustomMsgInfo;
 import com.android.gift.room.bean.RoomItem;
-import com.android.gift.room.doalog.InputKeyBoardDialog;
 import com.android.gift.room.view.VideoLiveControllerView;
 import com.android.gift.util.AppUtils;
-import com.android.gift.util.ScreenLayoutChangedHelp;
 import com.android.live.player.lib.manager.VideoPlayerManager;
 import com.android.live.player.lib.view.VideoPlayerTrack;
 
@@ -34,35 +27,37 @@ import com.android.live.player.lib.view.VideoPlayerTrack;
  * 礼物交互示例直播间
  */
 
-public class LiveRoomActivity extends AppCompatActivity {
+public class LiveRoomActivity extends BaseRoomActivity<VideoLiveControllerView> {
 
-    //礼物面板
-    private LiveGiftDialog mGiftDialog;
-    //直播间控制器
-    private VideoLiveControllerView mControllerView;
-    //输入框
-    private InputKeyBoardDialog mInputTextMsgDialog;
-    //输入框高度监听
-    private ScreenLayoutChangedHelp mLayoutChangedListener;
     private VideoPlayerTrack mPlayerTrack;
-    private String roomid;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getWindow();
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.TRANSPARENT);
-        }
-        //保持屏幕高亮，不锁屏
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_live_room);
-        //直播间交互控制器
-        mControllerView = findViewById(R.id.live_controller);
-        mControllerView.setFunctionListener(new VideoLiveControllerView.OnLiveRoomFunctionListener() {
+        //直播间控制器初始化
+        initRoomViews();
+        //视频初始化配置
+        VideoPlayerManager.getInstance().setLoop(true);
+        VideoPlayerManager.getInstance().setMobileWorkEnable(true);
+        mPlayerTrack = (VideoPlayerTrack) findViewById(R.id.video_track);
+        mPlayerTrack.setLoop(true);
+        onParams(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        onParams(intent);
+    }
+
+    @Override
+    protected void initRoomViews() {
+        mController = new VideoLiveControllerView(this);
+        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        mController.setLayoutParams(layoutParams);
+        super.initRoomViews();
+        mController.setFunctionListener(new VideoLiveControllerView.OnLiveRoomFunctionListener() {
             @Override
             public void backPress() {
                 finish();
@@ -70,22 +65,7 @@ public class LiveRoomActivity extends AppCompatActivity {
 
             @Override
             public void showGift(UserInfo anchorUser) {
-                if(null==mGiftDialog){
-                    mGiftDialog = LiveGiftDialog.getInstance(LiveRoomActivity.this);
-                    mGiftDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
-                        @Override
-                        public void onDismiss(DialogInterface dialog) {
-                            if(null!=mControllerView){
-                                mControllerView.closeGiftBoard();
-                            }
-                        }
-                    });
-                }
-                mGiftDialog.setReceiveUserInfo(anchorUser)
-                        .setReceiveRoomID(roomid)
-                        .setAutoSelectedEnable(true)
-                        .show();
-                mControllerView.showGiftBoard();
+                showGiftBoard(anchorUser);
             }
 
             @Override
@@ -127,31 +107,6 @@ public class LiveRoomActivity extends AppCompatActivity {
                 }
             }
         });
-        //视频初始化配置
-        VideoPlayerManager.getInstance().setLoop(true);
-        VideoPlayerManager.getInstance().setMobileWorkEnable(true);
-        mPlayerTrack = (VideoPlayerTrack) findViewById(R.id.video_track);
-        mPlayerTrack.setLoop(true);
-        //软键盘高度检测
-        mLayoutChangedListener= ScreenLayoutChangedHelp.get(LiveRoomActivity.this).setOnSoftKeyBoardChangeListener(new ScreenLayoutChangedHelp.OnSoftKeyBoardChangeListener() {
-            @Override
-            public void keyBoardShow(int height) {
-                if(null!=mControllerView) mControllerView.showInputKeyBord(true,height);
-            }
-
-            @Override
-            public void keyBoardHide(int height) {
-                if(null!=mControllerView) mControllerView.showInputKeyBord(false,height);
-            }
-        });
-        onParams(getIntent());
-    }
-
-
-    @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-        onParams(intent);
     }
 
     /**
@@ -166,11 +121,13 @@ public class LiveRoomActivity extends AppCompatActivity {
             finish();
             return;
         }
-        roomid=roomItem.getRoomid();
+        mRoomid=roomItem.getRoomid();
         //清屏
-        mControllerView.onReset();
-        mControllerView.setAnchorData(roomItem.getAnchor());
-        mControllerView.setOnLinesNumber(roomItem.getOnlineNumber());
+        if(null!=mController){
+            mController.onReset();
+            mController.setAnchorData(roomItem.getAnchor());
+            mController.setOnLinesNumber(roomItem.getOnlineNumber());
+        }
         //视频画面清屏
         VideoPlayerManager.getInstance().reset();
         mPlayerTrack.setVideoCover(roomItem.getRoom_front(),false);
@@ -182,62 +139,6 @@ public class LiveRoomActivity extends AppCompatActivity {
         newSystemCustomMessage(customInfo,false);
         //开始拉流
         mPlayerTrack.startPlayer(roomItem.getStream_url());
-    }
-
-    /**
-     * 收到新的消息交给控制器统一处理刷新
-     * 播放礼物动画
-     * @param customMsgInfo
-     * @param isSystemPro   是否来系统推送的
-     */
-    protected void newSystemCustomMessage(CustomMsgInfo customMsgInfo, boolean isSystemPro) {
-        if (null == customMsgInfo) return;
-        if (null != mControllerView) mControllerView.newSystemCustomMessage(customMsgInfo, isSystemPro);
-    }
-
-    /**
-     * 发消息弹出框
-     */
-    protected void showInputMsgDialog() {
-        if(null==mInputTextMsgDialog){
-            //输入框
-            mInputTextMsgDialog = InputKeyBoardDialog.getInstance(this)
-                    .setBackgroundWindown(0.0f)
-                    .setHintText("请输入聊天内容")
-                    .setMode(1)
-                    .setOnActionFunctionListener(new InputKeyBoardDialog.OnActionFunctionListener() {
-                        //提交发射
-                        @Override
-                        public void onSubmit(String content, boolean tanmuOpen) {
-                            CustomMsgInfo customMsgInfo=new CustomMsgInfo(0);
-                            customMsgInfo.setChildCmd(Constants.MSG_CUSTOM_TEXT);
-                            customMsgInfo.setMsgContent(content);
-                            customMsgInfo.setTanmu(tanmuOpen);
-                            if(null!=mControllerView){
-                                mControllerView.onNewTextMessage(customMsgInfo,false);
-                            }
-                        }
-                        //告知输入框是否可发射状态
-                        @Override
-                        public boolean isAvailable() {
-                            return true;
-                        }
-                    });
-            mInputTextMsgDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
-                @Override
-                public void onDismiss(DialogInterface dialog) {
-                    if(null!=mControllerView) mControllerView.showInputKeyBord(false,0);
-                }
-            });
-            WindowManager windowManager = getWindowManager();
-            Display display = windowManager.getDefaultDisplay();
-            WindowManager.LayoutParams lp = mInputTextMsgDialog.getWindow().getAttributes();
-            lp.width = (int) (display.getWidth()); //设置宽度
-            mInputTextMsgDialog.getWindow().setAttributes(lp);
-            mInputTextMsgDialog.setCancelable(true);
-            mInputTextMsgDialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
-        }
-        mInputTextMsgDialog.show();
     }
 
     @Override
@@ -263,24 +164,5 @@ public class LiveRoomActivity extends AppCompatActivity {
         getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         //震动持有释放
         VibratorManager.getInstance().onDestroy();
-        if(null!=mGiftDialog&&mGiftDialog.isShowing()){
-            mGiftDialog.destroy();
-            mGiftDialog.dismiss();
-            mGiftDialog=null;
-        }
-        //控制器机内部销毁，礼物动画回收也在此方法内部进行
-        if(null!=mControllerView){
-            mControllerView.onDestroy();
-            mControllerView=null;
-        }
-        if(null!= mLayoutChangedListener) {
-            mLayoutChangedListener.onDestroy();
-            mLayoutChangedListener=null;
-        }
-        if(null!=mInputTextMsgDialog){
-            mInputTextMsgDialog.destroy();
-            mInputTextMsgDialog.dismiss();
-            mInputTextMsgDialog=null;
-        }
     }
 }
